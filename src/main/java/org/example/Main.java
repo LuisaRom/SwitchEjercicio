f2c2eeb3fe9717efcb9bf9fbe354ed5c5feba75f
LuisaRom <lufromero13@gmail.com>
2025-02-24 16:23:13 -0500

    Switch Nuevo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,45 +1,38 @@
-package org.example;
 
 
-// SWITCH CASE ANTIGUO
-public class Main {
+//SWITCH NUEVO
+
+
     public static void main(String[] args) {
-
         int dia = 3;
-        String nombreDia;
 
-        switch (dia) {
-            case 1:
-                nombreDia = "Lunes";
-                break;
-            case 2:
-                nombreDia = "Martes";
-                break;
-            case 3:
-                nombreDia = "Miércoles";
-                break;
-            case 4:
-                nombreDia = "Jueves";
-                break;
-            case 5:
-                nombreDia = "Viernes";
-                break;
-            case 6:
-                nombreDia = "Sábado";
-                break;
-            case 7:
-                nombreDia = "Domingo";
-                break;
-            default:
-                nombreDia = "Día no válido";
-        }
+        String nombreDia = switch (dia) {
+            case 1 -> "Lunes";
+            case 2 -> "Martes";
+            case 3 -> "Miércoles";
+            case 4 -> "Jueves";
+            case 5 -> "Viernes";
+            case 6 -> "Sábado";
+            case 7 -> "Domingo";
+            default -> "Día no válido";
+        };
 
         System.out.println("El día es: " + nombreDia);
     }
-}
 
-// switch (dia) es la expresion que sera comparada en cada instancia case
-// si el nombre del dia dado coincide con case entonces se ejecuta la declaracion de switch
-// si se olvida el break, el script se ejecutará desde donde se cumple
-// la condición y ejecutará el siguiente case independientemente si esta condición se cumple o no.
+
+//Usa -> en lugar de : para asignaciones directas.
+//No necesita break, ya que solo se ejecuta un caso.
+
+
+
+
+
+
+
+
+
+
+
+
 
